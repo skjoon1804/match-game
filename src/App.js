2296b665a7e33bf0b84010f609f9e9ca6f566e7b
skjoon1804 <skjoon1804@naver.com>
2020-12-06 14:58:08 -0800

    lay out stars and numbers

--- src/App.js
+++ src/App.js
@@ -4,8 +4,33 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World!!!</h1>
+      <div className="game">
+        <div className="help">Click number(s) that sum to the number of stars</div>
+        <div className="body">
+          <div className="left">
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+          </div>
+          <div className="right">
+            <div className="number">1</div>
+            <div className="number">2</div>
+            <div className="number">3</div>
+            <div className="number">4</div>
+            <div className="number">5</div>
+            <div className="number">6</div>
+            <div className="number">7</div>
+            <div className="number">8</div>
+            <div className="number">9</div>
+          </div>
+        </div>
+        <div className="timer">Time Remaining: 10</div>
       </div>
     );
   }
