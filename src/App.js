ff735a652a5bc183db3a1e51a0fe4e5c10c8f603
skjoon1804 <skjoon1804@naver.com>
2020-12-09 00:24:30 -0800

    Changed App structure

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
 import './App.css';
-import React, {Component} from 'react';
+import React, {useEffect, useState, Component} from 'react';
 import StarsDisplay from './StarsDisplay';
 import PlayNumber from './PlayNumber';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      stars : Math.floor(Math.random()*9)+1,
-      availableNums : Array.from({length: 9}, (_, i) => 1+i),
-      candidateNums : [1,2,3]
-    };
+const App = () => {
+  const [stars, setStars] = useState(Math.floor(Math.random()*9)+1);
+  const [availableNums, setAvailableNums] = useState(Array.from({length: 9}, (_, i) => i+1));
+  const [candidateNums, setCandidateNums] = useState([]);
+
+  const candidatesAreWrong = () => {
+    let candidateSum = candidateNums.reduce((a,b) => a+b, 0);
+    return candidateSum > stars;
   }
 
-  candidatesAreWrong() {
-    let candidateSum = this.state.candidateNums.reduce((a,b) => a+b, 0);
-    console.log(candidateSum);
-    return candidateSum > this.state.stars; 
-  }
-
-  render() {
-    return (
+  return (
       <div className="game">
         <div className="help">Click number(s) that sum to the number of stars</div>
         <div className="body">
           <div className="left">
-            <StarsDisplay count={this.state.stars}/>
+            <StarsDisplay count={stars}/>
           </div>
           <div className="right">
             {Array.from({length: 9}, (_, i) => 1+i).map (number =>
                <PlayNumber status={'used'} key={number} number={number}
-                  onClick={this.candidatesAreWrong()}/>
+                  />
             )}
           </div>
         </div>
         <div className="timer">Time Remaining: 10</div>
       </div>
-    );
-  }
+  );
 }
-
 export default App;
