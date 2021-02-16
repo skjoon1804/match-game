9914b9508beb5f5c71f72a912fd5d2814399ac36
skjoon1804 <skjoon1804@naver.com>
2021-02-16 00:22:17 -0800

    Add game settings in progress

--- src/components/GameSettings/GameSettings.js
+++ src/components/GameSettings/GameSettings.js
@@ -1,22 +1,124 @@
 import React from 'react'
+import { connect } from 'react-redux';
 import './GameSettings.css'
 
 
+const GameSettings = ( {
 
-const GameSettings = props => (
+    startEasy, startMedium, startHard, startCrazy
+}) => (
     <>
-    <div className='settings'>
-        <form id="frm">
-            <label>
-                Number of Stars: <input type="text" name="numstars" required/>
-            </label>
-            <label>
-                Timer: <input type="text" name="timer" required/>
-            </label>
-            
-            <input type="submit" className="start-game" onClick={props.onClick} value="Start Game" />
-        </form>
+    <div className='container h-100 m-auto'>
+        <div className="row">
+            <div className="level col-sm card p-0">
+                <div className="card-header">
+                    <h3 className="level-title text-center display-4">Easy</h3>
+                </div>
+                <div className="card-body">
+                    <div className="level-desc py-5">
+                        <div className="row justify-content-center">
+                            <div className="col-6">Time</div>
+                            <div className="col-6">25 Seconds</div>
+                        </div>
+                        <div className="row justify-content-center">
+                            <div className="col-6">Stars</div>
+                            <div className="col-6">9</div>
+                        </div>               
+                    </div>
+                    <div className="text-center m-2">
+                        <button className="btn btn-success form-control" onClick={() => {}}>GO</button>
+                    </div>
+                </div>
+            </div>
+            <div className="level col-sm card p-0">
+                <div className="card-header">
+                    <h3 className="level-title text-center display-4">Medium</h3>
+                </div>
+                <div className="card-body">
+                    <div className="level-desc py-5">
+                        <div className="row justify-content-center">
+                            <div className="col-6">Time</div>
+                            <div className="col-6">20 Seconds</div>
+                        </div>
+                        <div className="row justify-content-center">
+                            <div className="col-6">Stars</div>
+                            <div className="col-6">16</div>
+                        </div>               
+                    </div>
+                    <div className="text-center m-2">
+                        <button className="btn btn-primary form-control" onClick={() => {}}>GO</button>
+                    </div>
+                </div>
+            </div>
+            <div className="level col-sm card p-0">
+                <div className="card-header">
+                    <h3 className="level-title text-center display-4">Hard</h3>
+                </div>
+                <div className="card-body">
+                    <div className="level-desc py-5">
+                        <div className="row justify-content-center">
+                            <div className="col-6">Time</div>
+                            <div className="col-6">15 Seconds</div>
+                        </div>
+                        <div className="row justify-content-center">
+                            <div className="col-6">Stars</div>
+                            <div className="col-6">16</div>
+                        </div>               
+                    </div>
+                    <div className="text-center m-2">
+                        <button className="btn btn-danger form-control" onClick={() => {}}>GO</button>
+                    </div>
+                </div>
+            </div>
+            <div className="level col-sm card p-0">
+                <div className="card-header">
+                    <h3 className="level-title text-center display-4">Crazy</h3>
+                </div>
+                <div className="card-body">
+                    <div className="level-desc py-5">
+                        <div className="row justify-content-center">
+                            <div className="col-6">Time</div>
+                            <div className="col-6">15 Seconds</div>
+                        </div>
+                        <div className="row justify-content-center">
+                            <div className="col-6">Stars</div>
+                            <div className="col-6">25</div>
+                        </div>               
+                    </div>
+                    <div className="text-center m-2">
+                        <button className="btn btn-dark form-control" onClick={() => {}}>GO</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </div>
     </>
 );
-export default GameSettings
+
+const mapStateToProps = (state) => {
+    return {
+        state
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+
+    return {
+        startEasy() {
+
+        },
+        startMedium() {
+
+        },
+        startHard() {
+
+        },
+        startCrazy() {
+
+        }
+    }
+}
+
+export const ConnectedGameSettings = connect(mapStateToProps, mapDispatchToProps)(GameSettings);
