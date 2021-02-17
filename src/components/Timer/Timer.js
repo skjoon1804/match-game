c65ec748d57c6d00f0884c315d1e3ff949f70499
skjoon1804 <skjoon1804@naver.com>
2021-02-17 00:41:15 -0800

    Add timer animation

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './Timer.css'
 
-const Timer = (props) => {
-
-    let timerPercent = (props.secondsLeft/5)*100 + "%";
+const Timer = ({
+    time, secondsLeft
+}) => {
     
-
+    let timerPercent = (secondsLeft/time)*100 + "%";
     return (
         <div className="m-2">
-            <div className="timer">Time Remaining: {props.secondsLeft}</div>
+            <div className="timer">Time Remaining: {secondsLeft}</div>
             <div className="progress">
-                <div className="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow={props.secondsLeft} 
-                        aria-valuemin="0" aria-valuemax="5" style={{width: timerPercent}}>
+                <div className="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow={secondsLeft} 
+                        aria-valuemin="0" aria-valuemax={time} style={{width: timerPercent}}>
                 </div>
             </div>
         </div>
     )
 }
-export default Timer;
+
+const mapStateToProps = (state, ownProps) => {
+    let secondsLeft = ownProps.secondsLeft;
+    let time = state.time;
+    return { time, secondsLeft };
+}
+
+export const ConnectedTimer = connect(mapStateToProps)(Timer);
