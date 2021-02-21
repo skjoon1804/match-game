a3868a773e636a8e0603faf0d501fba896563722
skjoon1804 <skjoon1804@naver.com>
2021-02-21 01:14:34 -0800

    Add initialize db with default state

---
+++ src/server/defaultState.js
@@ -0,0 +1,24 @@
+export const defaultState = {
+    record: [
+        {
+            level: 'easy',
+            record: []
+        },
+        {
+            level: 'medium',
+            record: []
+        },
+        {
+            level: 'hard',
+            record:[]
+        },
+        {
+            level: 'crazy',
+            record: [{
+                level: 'crazy',
+                name: 'test',
+                score: 100000
+            }]
+        }
+    ]
+}
