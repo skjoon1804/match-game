6666a43e5c3ba2c98919fb9c48dbb3811eb00380
skjoon1804 <skjoon1804@naver.com>
2021-02-22 20:01:05 -0800

    Consolidate server side post response

--- src/server/server.js
+++ src/server/server.js
@@ -23,98 +23,32 @@ if (process.env.NODE_ENV == `production`) {
     });
 }
 
-export const addEasyRecord = async easy => {
-    let { level, name, score } = easy;
+export const addRecord = async record => {
+    let { level, name, score } = record;
     let db = await connectDB();
     let collection = db.collection(`record`);
 
     await collection.updateOne({level}, {$push: {record: {level, name, score}}});
-    let updatedEasy = await collection.find({level}).toArray();
-    
-    let size = updatedEasy[0].record.length;
+    let updated = await collection.find({level}).toArray();
+
+    let size = updated[0].record.length;
     if (size > 10) {
-        let sortedEasy = updatedEasy[0].record.sort((a,b) => { return b.score - a.score; });
-        let min = sortedEasy[sortedEasy.length - 1];
-        await collection.updateOne({level}, {$pull: {record: {name: min.name, score: min.score}}});
-    }
-}
-
-export const addMediumRecord = async medium => {
-    let { level, name, score } = medium;
-    let db = await connectDB();
-    let collection = db.collection(`record`);
-
-    await collection.updateOne({level}, {$push: {record: {level, name, score}}});
-    let updatedEasy = await collection.find({level}).toArray();
-
-    let size = updatedEasy[0].record.length;
-    if (size > 10) {
-        let sortedEasy = updatedEasy[0].record.sort((a,b) => { return b.score - a.score; });
-        let min = sortedEasy[sortedEasy.length - 1];
-        await collection.updateOne({level}, {$pull: {record: {name: min.name, score: min.score}}});
-    }
-}
-
-export const addHardRecord = async hard => {
-    let { level, name, score } = hard;
-    let db = await connectDB();
-    let collection = db.collection(`record`);
-
-    await collection.updateOne({level}, {$push: {record: {level, name, score}}});
-    let updatedEasy = await collection.find({level}).toArray();
-
-    let size = updatedEasy[0].record.length;
-    if (size > 10) {
-        let sortedEasy = updatedEasy[0].record.sort((a,b) => { return b.score - a.score; });
-        let min = sortedEasy[sortedEasy.length - 1];
-        await collection.updateOne({level}, {$pull: {record: {name: min.name, score: min.score}}});
-    }
-}
-
-export const addCrazyRecord = async crazy => {
-    let { level, name, score } = crazy;
-    let db = await connectDB();
-    let collection = db.collection(`record`);
-
-    await collection.updateOne({level}, {$push: {record: {level, name, score}}});
-    let updatedEasy = await collection.find({level}).toArray();
-
-    let size = updatedEasy[0].record.length;
-    if (size > 10) {
-        let sortedEasy = updatedEasy[0].record.sort((a,b) => { return b.score - a.score; });
-        let min = sortedEasy[sortedEasy.length - 1];
+        let sorted = updated[0].record.sort((a,b) => { return b.score - a.score; });
+        let min = sorted[sorted.length - 1];
         await collection.updateOne({level}, {$pull: {record: {name: min.name, score: min.score}}});
     }
 }
 
 // -----------------------
 
-app.post('/record', async (req, res) => {
+app.post('/record/get', async (req, res) => {
     let db = await connectDB();
     let record = await db.collection(`record`).find().toArray();
     res.status(200).send({record});
 })
 
-app.post('/easy', async (req, res) => {
+app.post('/record/add', async (req, res) => {
     let record = req.body.record;
-    await addEasyRecord(record);
-    res.status(200).send();
-})
-
-app.post('/medium', async (req, res) => {
-    let medium = req.body.medium;
-    await addMediumRecord(medium);
-    res.status(200).send();
-})
-
-app.post('/hard', async (req, res) => {
-    let hard = req.body.hard;
-    await addHardRecord(hard);
-    res.status(200).send();
-})
-
-app.post('/crazy', async (req, res) => {
-    let crazy = req.body.crazy;
-    await addCrazyRecord(crazy);
+    await addRecord(record);
     res.status(200).send();
 })
